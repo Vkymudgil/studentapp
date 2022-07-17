7d8c2b58792365d595ea15c948edbe08b9e1229a
virtualemployee300 <51941847+Vkymudgil@users.noreply.github.com>
2022-07-17 13:34:06 +0530

    Update 1

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require ('express');
 const app = express();
+const fs = require('fs');
+
+app.use(express.json());
 
 app.get('/hello', (req,res) => {
     res.send("Hello World!");
 })
 
+app.post('/student', (req,res) => {
+  console.log(req.body);
+  let data = JSON.stringify(req.body);
+  fs.appendFile('student.txt', data , (err) => {
+    if(err){
+      console.log(err);
+    } else {
+      res.send('Data Saved In File');
+    }
+  });
+})
 
 
 app.listen(3000, ()=> {
